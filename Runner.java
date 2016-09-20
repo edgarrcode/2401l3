dab84eb179d5ab6afa26339a4dd9c5db1071565a
Edgar Rodriguez <1983eerr@gmail.com>
2016-09-19 20:49:53 -0600

    firstt

---
+++ Runner.java
@@ -0,0 +1,17 @@
+/*
+Author: Edgar E. Rodriguez
+TAs: Anthony M Ortiz Cepeda and Khandoker A Rahad
+Professor: Mahmud Hossain
+LMD: 09/15/16
+Goals: Read the widht, height, and length of packages from file, and calculate the following:
+• 
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Runner {
+  
+  public static Box readFile
+
+}
