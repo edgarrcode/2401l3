275dea5417241f74c4ead0137973dc7a379996a6
Edgar Rodriguez <1983eerr@gmail.com>
2016-09-21 23:11:27 -0600

    how many cubic boxes, smallest cubic box, largest cubic box

--- Runner.java
+++ Runner.java
@@ -85,6 +85,87 @@ public class Runner {
         return smallestBoxPos;
     }
 
+    //Find the largest box. Report position, dimensions, and volume of the smallest object.
+    public static int largestBoxPos (BoxLL head) {
+        // set largestBoxPos to first item of list and largestVolume to volume of first item of the list
+        int largestBoxPos = 0;
+        double largestVolume = head.getTheBox().getVolume();;
+
+        //get values of next LL
+        BoxLL tempLL = new BoxLL();
+        tempLL = head.next;
+        int counter = 1;
+
+        while(tempLL != null) {
+            if ((tempLL.getTheBox().getVolume()) > largestVolume) {
+                largestVolume = tempLL.getTheBox().getVolume();
+                largestBoxPos = counter;
+            }
+            tempLL = tempLL.next;
+            counter++;
+        }
+
+        return largestBoxPos;
+    }
+
+    //--- How many cubic boxes are there in the linked list?
+    public static int howManyCubicBoxes(BoxLL head) {
+        int howManyCubicBoxes = 0;
+
+        while(head != null) {
+            if ((head.getTheBox().getWidth() == head.getTheBox().getHeight()) && (head.getTheBox().getHeight() == head.getTheBox().getLength())) {
+                howManyCubicBoxes++;
+            }
+            head = head.next;
+        }
+
+        return howManyCubicBoxes;
+    }
+
+    //--- Position, dimensions, and volume of the smallest cubic box
+    public static int smallestCubicBoxPos(BoxLL head) {
+        // set smallestCubicBoxPos to -1 in case there are no cubic boxes
+        int smallestCubicBoxPos = -1;
+        int counter = 0;
+        double smallestCubicVolume = Double.POSITIVE_INFINITY;
+
+        while(head != null) {
+            if ((head.getTheBox().getWidth() == head.getTheBox().getHeight()) && (head.getTheBox().getHeight() == head.getTheBox().getLength())) {
+                if (smallestCubicVolume > head.getTheBox().getVolume()) {
+                    smallestCubicVolume = head.getTheBox().getVolume();
+                    smallestCubicBoxPos = counter;
+                }
+            }
+            head = head.next;
+            counter++;
+        }
+
+        return smallestCubicBoxPos;
+    }
+
+    //--- Position, dimensions, and volume of the largest cubic box
+    public static int largestCubicBoxPos(BoxLL head) {
+        // set largestCubicBoxPos to -1 in case there are no cubic boxes
+        int largestCubicBoxPos = -1;
+        int counter = 0;
+        double largestCubicVolume = 0;
+
+        while(head != null) {
+            if ((head.getTheBox().getWidth() == head.getTheBox().getHeight()) && (head.getTheBox().getHeight() == head.getTheBox().getLength())) {
+                if (largestCubicVolume < head.getTheBox().getVolume()) {
+                    largestCubicVolume = head.getTheBox().getVolume();
+                    largestCubicBoxPos = counter;
+                }
+            }
+            head = head.next;
+            counter++;
+        }
+
+        return largestCubicBoxPos;
+    }
+
+
+
     public static void main (String[] args) throws IOException {
         // ask for file name to build LL
         String fileName = "";
@@ -116,6 +197,67 @@ public class Runner {
         System.out.println("The height of the smallest package is: " + tempLL.getTheBox().getHeight());
         System.out.println("The length of the smallest package is: " + tempLL.getTheBox().getLength());
         System.out.println("The volume of the smallest package is: " + tempLL.getTheBox().getVolume());
+        System.out.print("\n");
+
+        //--- Find the largest box. Report the position, dimensions, and volume of the largest object.
+        int largestBoxPos = largestBoxPos(head); //get the index position
+        tempLL = head;
+        //iterate to correct position
+        for (int i = 0; i < largestBoxPos; i++) {
+            tempLL = tempLL.next;
+        }
+        System.out.println("The position of the largest package is: " + largestBoxPos);
+        System.out.println("The width of the largest package is: " + tempLL.getTheBox().getWidth());
+        System.out.println("The height of the largest package is: " + tempLL.getTheBox().getHeight());
+        System.out.println("The length of the largest package is: " + tempLL.getTheBox().getLength());
+        System.out.println("The volume of the largest package is: " + tempLL.getTheBox().getVolume());
+        System.out.print("\n");
+
+        //--- How many cubic boxes are there in the linked list?
+        int howManyCubicBoxes = howManyCubicBoxes(head);
+        System.out.println("Amount of cubic boxes: " + howManyCubicBoxes);
+        System.out.print("\n");
+
+
+        //--- Position, dimensions, and volume of the smallest cubic box
+        int smallestCubicBoxPos = smallestCubicBoxPos(head);
+        if (smallestCubicBoxPos < 0) {
+            System.out.println("Can't find the smallest cubic box because there are no cubic packages.");
+            System.out.print("\n");
+        } else {
+            tempLL = head;
+            // iterate to the correct position
+            for (int i = 0; i < smallestCubicBoxPos; i++) {
+                tempLL = tempLL.next;
+            }
+            System.out.println("The position of the smallest cubic package is: " + smallestCubicBoxPos);
+            System.out.println("The width of the smallest cubic package is: " + tempLL.getTheBox().getWidth());
+            System.out.println("The height of the smallest cubic package is: " + tempLL.getTheBox().getHeight());
+            System.out.println("The length of the smallest cubic package is: " + tempLL.getTheBox().getLength());
+            System.out.println("The volume of the smallest cubic package is: " + tempLL.getTheBox().getVolume());
+            System.out.print("\n");
+        }
+
+        //--- Position, dimensions, and volume of the largest cubic box
+        int largestCubicBoxPos = largestCubicBoxPos(head);
+        if (largestCubicBoxPos < 0) {
+            System.out.println("Can't find the largest cubic box because there are no cubic packages.");
+            System.out.print("\n");
+        } else {
+            tempLL = head;
+            // iterate to the correct position
+            for (int i = 0; i < largestCubicBoxPos; i++) {
+                tempLL = tempLL.next;
+            }
+            System.out.println("The position of the largest cubic package is: " + largestCubicBoxPos);
+            System.out.println("The width of the largest cubic package is: " + tempLL.getTheBox().getWidth());
+            System.out.println("The height of the largest cubic package is: " + tempLL.getTheBox().getHeight());
+            System.out.println("The length of the largest cubic package is: " + tempLL.getTheBox().getLength());
+            System.out.println("The volume of the largest cubic package is: " + tempLL.getTheBox().getVolume());
+            System.out.print("\n");
+        }
+
+
 
     }
 
